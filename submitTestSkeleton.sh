32d22cc7578e668707466d8ebb807685c3cc50bc
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2016-10-31 21:54:54 +0100

    Add local batch submition stuff

---
+++ submitTestSkeleton.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -q express
+
+# setting up your code and your env
+source /user/dlontkov/.bash_profile
+cd /user/dlontkov/CMSSW_7_6_3/src/TopBrussels/FourTops_dev1
+eval `scramv1 runtime -sh`
+
+# want you really want to do!!
+
