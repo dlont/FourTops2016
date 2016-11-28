f57546e2192d0f323044d97c05c9c8f51f91d18b
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2016-11-28 15:30:06 +0100

    Add dcap://maite.iihe.ac.be to file names in order to avoid slowness. Add summaru output at the end

--- config/xmlFiller.py
+++ config/xmlFiller.py
@@ -12,6 +12,8 @@ import xml.etree.cElementTree as ET
 inputFile = sys.argv[1]
 outputFileName = 'output_'+inputFile
 
+summary = []
+
 tree = ET.ElementTree(file=inputFile)
 root = tree.getroot()
 datasets = root.find('datasets')
@@ -19,18 +21,22 @@ for d in datasets:
     if 'data' in d.attrib['name'].lower():
         d.set('EqLumi','1')
         continue
+    if d.attrib['add'] == 0:
+	continue
     xsec = float(d.attrib['xsection'])
     files = glob.glob(d.attrib['filenames'])
     print files
     root_files = []
-    #for f in files:
-        #root_files.append('dcap://maite.iihe.ac.be'+f)
+    for f in files:
+        root_files.append('dcap://maite.iihe.ac.be'+f)
     chain = TChain('eventTree')
     for rf in root_files:
         chain.Add(rf)
     nEntries = chain.GetEntries()
     equivLumi = nEntries/xsec
     d.set('EqLumi',str(equivLumi))
-    print d.attrib['name']
+    summary.append( [d.attrib['name'], nEntries, equivLumi] )
 print 'filled xml with eqlumis!'
+for item in summary:
+	print item
 tree.write(outputFileName)
