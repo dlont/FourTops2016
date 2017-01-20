8873f41c49d059e96de24887e974aac92ae1b59f
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-01-20 03:58:37 +0100

    Add ommitted Makefile include, without which craneen mearging doesn't work

---
+++ result/Makefile_merge.mk
@@ -0,0 +1,48 @@
+$(BUILDDIR)/Craneen_Data_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_Data*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_ttttNLO_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_ttttNLO*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_TTJets_powheg_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_TTJets_powheg*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_TTISRScaledown_powheg_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_TTISRScaledown_powheg*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_TTISRScaleup_powheg_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_TTISRScaleup_powheg*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_TTFSRScaledown_powheg_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_TTFSRScaledown_powheg*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_TTFSRScaleup_powheg_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_TTFSRScaleup_powheg*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_TTUETunedown_powheg_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_TTUETunedown_powheg*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_TTUETuneup_powheg_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_TTUETuneup_powheg*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_T_tW_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_T_tW*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_Tbar_tW_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_Tbar_tW*.root)
+	@echo merging $@
+	@hadd $@ $^
+
+$(BUILDDIR)/Craneen_WJets_Run2_TopTree_Study.root: $(wildcard $(INPUTLOCATION)/Craneen_WJets*.root)
+	@echo merging $@
+	@hadd $@ $^
+
