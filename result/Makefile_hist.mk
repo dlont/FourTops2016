a21ac23dff3b33af2e77729b1d56f4539ed4e11b
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-01-20 03:54:53 +0100

    Improve Makefile. Fix potential issues with paths

--- result/Makefile_hist.mk
+++ result/Makefile_hist.mk
@@ -26,7 +26,7 @@ $(BUILDDIR)/Hists_Tbar_tW.root: config_t2h.py $(BUILDDIR)/Craneen_Tbar_tW_Run2_T
 
 $(BUILDDIR)/Hists_SYST.root: $(BUILDDIR)/Hists_TT.root $(BUILDDIR)/Hists_tuneup.root $(BUILDDIR)/Hists_tunedown.root $(BUILDDIR)/Hists_isrscaleup.root $(BUILDDIR)/Hists_isrscaledown.root $(BUILDDIR)/Hists_fsrscaleup.root $(BUILDDIR)/Hists_fsrscaledown.root
 	@echo "Preparing TT scale systematic histograms"
-	@tools/scalehist.py -o $@ --nominal=$(dir $@)/Hists_TT.root --isrscaleup=$(dir $@)/Hists_isrscaleup.root --isrscaledown=$(dir $@)/Hists_isrscaledown.root
+	@tools/scalehist.py -o $@ --nominal=$(BUILDDIR)/Hists_TT.root --isrscaleup=$(BUILDDIR)/Hists_isrscaleup.root --isrscaledown=$(BUILDDIR)/Hists_isrscaledown.root
 
 $(BUILDDIR)/Hists_TT.root: config_t2h.py $(BUILDDIR)/Craneen_TTJets_powheg_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
