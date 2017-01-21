33622b93ad27a704a12f4a744b6c046a93fa3370
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-01-21 13:47:52 +0100

    intorduce output suppression

--- result/Makefile_hist.mk
+++ result/Makefile_hist.mk
@@ -1,28 +1,31 @@
 #Order of arguments matters (first config.py, second input_tree.root, third scale)
 include Makefile_norm.mk
+
+SUPPRESSOUT=>/dev/null
+
 $(BUILDDIR)/Hists_data.root: config_t2h.py $(BUILDDIR)/Craneen_Data_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@" 
-	@tree2hists $^ $@ ${TREENAME} ${DATANORM}
+	@tree2hists $^ $@ ${TREENAME} ${DATANORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_TTTT.root: config_t2h.py $(BUILDDIR)/Craneen_ttttNLO_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@" 
-	@tree2hists $^ $@ ${TREENAME}  ${TTTTNORM}
+	@tree2hists $^ $@ ${TREENAME}  ${TTTTNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_WJets.root: config_t2h.py $(BUILDDIR)/Craneen_WJets_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}  ${WJETSNORM}
+	@tree2hists $^ $@ ${TREENAME}  ${WJETSNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_T.root: $(BUILDDIR)/Hists_T_tW.root $(BUILDDIR)/Hists_Tbar_tW.root
 	@echo "Merging single top histograms"
-	@hadd -f $@ $^
+	@hadd -f $@ $^ ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_T_tW.root: config_t2h.py $(BUILDDIR)/Craneen_T_tW_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}  ${TNORM}
+	@tree2hists $^ $@ ${TREENAME}  ${TNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_Tbar_tW.root: config_t2h.py $(BUILDDIR)/Craneen_Tbar_tW_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}   ${TBARNORM}
+	@tree2hists $^ $@ ${TREENAME}   ${TBARNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_SYST.root: $(BUILDDIR)/Hists_TT.root $(BUILDDIR)/Hists_tuneup.root $(BUILDDIR)/Hists_tunedown.root $(BUILDDIR)/Hists_isrscaleup.root $(BUILDDIR)/Hists_isrscaledown.root $(BUILDDIR)/Hists_fsrscaleup.root $(BUILDDIR)/Hists_fsrscaledown.root
 	@echo "Preparing TT scale systematic histograms"
@@ -30,30 +33,30 @@ $(BUILDDIR)/Hists_SYST.root: $(BUILDDIR)/Hists_TT.root $(BUILDDIR)/Hists_tuneup.
 
 $(BUILDDIR)/Hists_TT.root: config_t2h.py $(BUILDDIR)/Craneen_TTJets_powheg_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}  ${TTNORM}
+	@tree2hists $^ $@ ${TREENAME}  ${TTNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_tuneup.root: config_t2h.py $(BUILDDIR)/Craneen_TTUETuneup_powheg_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}  ${TTNORM}
+	@tree2hists $^ $@ ${TREENAME}  ${TTNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_tunedown.root: config_t2h.py $(BUILDDIR)/Craneen_TTUETunedown_powheg_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}  ${TTNORM}
+	@tree2hists $^ $@ ${TREENAME}  ${TTNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_isrscaleup.root: config_t2h.py $(BUILDDIR)/Craneen_TTISRScaleup_powheg_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}   ${TTNORM}
+	@tree2hists $^ $@ ${TREENAME}   ${TTNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_isrscaledown.root: config_t2h.py $(BUILDDIR)/Craneen_TTISRScaledown_powheg_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}   ${TTNORM}
+	@tree2hists $^ $@ ${TREENAME}   ${TTNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_fsrscaleup.root: config_t2h.py $(BUILDDIR)/Craneen_TTFSRScaleup_powheg_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}   ${TTNORM}
+	@tree2hists $^ $@ ${TREENAME}   ${TTNORM} ${SUPPRESSOUT}
 
 $(BUILDDIR)/Hists_fsrscaledown.root: config_t2h.py $(BUILDDIR)/Craneen_TTFSRScaledown_powheg_Run2_TopTree_Study.root
 	@echo "Convert tree to hist $@"
-	@tree2hists $^ $@ ${TREENAME}   ${TTNORM}
+	@tree2hists $^ $@ ${TREENAME}   ${TTNORM} ${SUPPRESSOUT}
 
 #@if [ -d "$(dir $@)" ]; then echo "$(BUILDDIR) exists" ; else mkdir $@ ; fi
