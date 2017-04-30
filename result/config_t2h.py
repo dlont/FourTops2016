d5c0c137ed5348b2a1de5e9b2ed5078a62de4fbf
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-04-30 21:38:54 +0200

    Add trigger cuts

--- result/config_t2h.py
+++ result/config_t2h.py
@@ -19,7 +19,13 @@ scalefactor=float(sys.argv[5])
 print 'Using input file: ' + inputfile
 print 'Output file: ' + inputfile
 
-list_of_files = [RootTree(str(tree_name), fileName=inputfile, scale=scalefactor, cuts="(HLT_IsoMu24==1||HLT_IsoTkMu24==1)")]
+trigger_cuts = ''
+if 'Mu' in tree_name:
+	trigger_cuts = "(HLT_IsoMu24==1||HLT_IsoTkMu24==1)"
+elif 'El' in tree_name:
+	trigger_cuts = "HLT_Ele32_eta2p1_WPTight_Gsf==1"
+
+list_of_files = [RootTree(str(tree_name), fileName=inputfile, scale=scalefactor, cuts="")]
 
 cut_for_all_files = ""
 
@@ -28,18 +34,18 @@ cut_for_all_files = ""
 # Let cut_sets = [] to make all plots.
 cut_sets = [
     ("allSF", "", "ScaleFactor"),
-    #("6J2M", "Njet=6, nMtags=2", "(nJets==6 && nMtags==2)*ScaleFactor"),
-    #("6J3M", "Njet=6, nMtags=3", "(nJets==6 && nMtags==3)*ScaleFactor"),
-    #("6J4M", "Njet=6, nMtags=4", "(nJets==6 && nMtags>=4)*ScaleFactor"),
-    #("7J2M", "Njet=7, nMtags=2", "(nJets==7 && nMtags==2)*ScaleFactor"),
-    #("7J3M", "Njet=7, nMtags=3", "(nJets==7 && nMtags==3)*ScaleFactor"),
-    #("7J4M", "Njet=7, nMtags=4", "(nJets==7 && nMtags>=4)*ScaleFactor"),
-    #("8J2M", "Njet=8, nMtags=2", "(nJets==8 && nMtags==2)*ScaleFactor"),
-    #("8J3M", "Njet=8, nMtags=3", "(nJets==8 && nMtags==3)*ScaleFactor"),
-    #("8J4M", "Njet=8, nMtags=4", "(nJets==8 && nMtags>=4)*ScaleFactor"),
-    #("9J2M", "Njet=9+, nMtags=2", "(nJets>=9 && nMtags==2)*ScaleFactor"),
-    #("9J3M", "Njet=9+, nMtags=3", "(nJets>=9 && nMtags==3)*ScaleFactor"),
-    #("9J4M", "Njet=9+, nMtags=4", "(nJets>=9 && nMtags>=4)*ScaleFactor"),
+    ("6J2M", "Njet=6, nMtags=2",  "(nJets==6 && nMtags==2)*ScaleFactor*"+trigger_cuts),
+    ("6J3M", "Njet=6, nMtags=3",  "(nJets==6 && nMtags==3)*ScaleFactor*"+trigger_cuts),
+    ("6J4M", "Njet=6, nMtags=4",  "(nJets==6 && nMtags>=4)*ScaleFactor*"+trigger_cuts),
+    ("7J2M", "Njet=7, nMtags=2",  "(nJets==7 && nMtags==2)*ScaleFactor*"+trigger_cuts),
+    ("7J3M", "Njet=7, nMtags=3",  "(nJets==7 && nMtags==3)*ScaleFactor*"+trigger_cuts),
+    ("7J4M", "Njet=7, nMtags=4",  "(nJets==7 && nMtags>=4)*ScaleFactor*"+trigger_cuts),
+    ("8J2M", "Njet=8, nMtags=2",  "(nJets==8 && nMtags==2)*ScaleFactor*"+trigger_cuts),
+    ("8J3M", "Njet=8, nMtags=3",  "(nJets==8 && nMtags==3)*ScaleFactor*"+trigger_cuts),
+    ("8J4M", "Njet=8, nMtags=4",  "(nJets==8 && nMtags>=4)*ScaleFactor*"+trigger_cuts),
+    ("9J2M", "Njet=9+, nMtags=2", "(nJets>=9 && nMtags==2)*ScaleFactor*"+trigger_cuts),
+    ("9J3M", "Njet=9+, nMtags=3", "(nJets>=9 && nMtags==3)*ScaleFactor*"+trigger_cuts),
+    ("9J4M", "Njet=9+, nMtags=4", "(nJets>=9 && nMtags>=4)*ScaleFactor*"+trigger_cuts),
     #("PU", "", "SFPU"),
     #("PUup", "", "SFPU_up"),
     #("PUdown", "", "SFPU_down"),
@@ -48,12 +54,12 @@ cut_sets = [
     #("toprew", "", "toprew"),
     #("csvrs", "", "csvrsw[0]"),
     #("noSF", "", ""),
-    ("weight1", "", "weight1*ScaleFactor"),
-    ("weight2", "", "weight2*ScaleFactor"),
-    ("weight3", "", "weight3*ScaleFactor"),
-    ("weight4", "", "weight4*ScaleFactor"),
-    ("weight6", "", "weight6*ScaleFactor"),
-    ("weight8", "", "weight8*ScaleFactor"),
+    ("weight1", "", "(weight1*ScaleFactor)*"+trigger_cuts),
+    ("weight2", "", "(weight2*ScaleFactor)*"+trigger_cuts),
+    ("weight3", "", "(weight3*ScaleFactor)*"+trigger_cuts),
+    ("weight4", "", "(weight4*ScaleFactor)*"+trigger_cuts),
+    ("weight6", "", "(weight6*ScaleFactor)*"+trigger_cuts),
+    ("weight8", "", "(weight8*ScaleFactor)*"+trigger_cuts),
 #    (weight7"barrel15to20", "(|#eta|<1.45weight7, 15<E_{T}<20)", "et>15&&et<20&&abs(eta)<1.45"),
 #    ("barrel20to30", "(|#eta|<1.45, 20<E_{T}<30)", "et>20&&et<30&&abs(eta)<1.45"),
 #    ("endcap15to20", "(1.7<|#eta|<2.5, 15<E_{T}<20)", "et>15&&et<20&&abs(eta)>1.7&&abs(eta)<2.5"),
