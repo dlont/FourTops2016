d240080f5a83d3805c51d59542a4cbe32fcb6b88
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-06-03 08:30:08 +0200

    Fix names of the output files

--- result/Makefile_sys.mk
+++ result/Makefile_sys.mk
@@ -4,6 +4,7 @@ DIFFNUIS=~/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py
 
 #MINOS=--minos=all
 MINOS=
+RUN=blind
 
 sysplots: 	$(BUILDDIR)/Hists_TT_CARDS.root $(BUILDDIR)/Hists_TTTT_CARDS.root
 	@python -b $(SYSPLOTDIR)/sysplot.py -b $(BUILDDIR)/Hists_TT_CARDS.root -j $(SYSPLOTDIR)/JES_tt.json -r $(BUILDDIR)/Hists_TT_CARDS.root -f $(FORMAT)
@@ -29,9 +30,9 @@ combinechecks: card_mu.txt card_el.txt datacard_elmu.txt
 	@combine -M MaxLikelihoodFit -t -1 --expectSignal 1 --robustFit=true $(MINOS) --out cards_mu card_mu.txt -n _AsymovMu1
 	@python $(DIFFNUIS) --absolute -a cards_mu/mlfit_AsymovMu1.root -g cards_mu/plots_AsymovMu1.root
 	@echo "Asimov check: Single electron"
-	@combine -M MaxLikelihoodFit -t -1 --expectSignal 0 --robustFit=true $(MINOS) --out cards_el card_el.txt -n _AsymovMu0
+	@combine -M MaxLikelihoodFit -t -1 --expectSignal 0 --robustFit=true $(MINOS) --out cards_el card_el.txt -n _AsymovEl0
 	@python $(DIFFNUIS) --absolute -a cards_el/mlfit_AsymovEl0.root -g cards_el/plots_AsymovEl0.root
-	@combine -M MaxLikelihoodFit -t -1 --expectSignal 1 --robustFit=true $(MINOS) --out cards_el card_el.txt -n _AsymovMu1
+	@combine -M MaxLikelihoodFit -t -1 --expectSignal 1 --robustFit=true $(MINOS) --out cards_el card_el.txt -n _AsymovEl1
 	@python $(DIFFNUIS) --absolute -a cards_el/mlfit_AsymovEl1.root -g cards_el/plots_AsymovEl1.root
 	@echo "Pulls and correlations"
 	@combine -M MaxLikelihoodFit --robustFit=true $(MINOS) --plots --saveShapes datacard_elmu.txt -n _CorrComb
