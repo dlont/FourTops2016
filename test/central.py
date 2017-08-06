25bde896a550f9ce9558fcd41646522a9a4209c4
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-08-06 18:45:39 +0200

    Add tests for cards histograms

---
+++ test/central.py
@@ -0,0 +1,39 @@
+central = [
+
+        {
+            'Name' : '/7J2M/bdt',
+        },
+        {
+            'Name' : '/7J3M/bdt',
+        },
+        {
+            'Name' : '/7J4M/bdt',
+        },
+        {
+            'Name' : '/8J2M/bdt',
+        },
+        {
+            'Name' : '/8J3M/bdt',
+        },
+        {
+            'Name' : '/8J4M/bdt',
+        },
+        {
+            'Name' : '/9J2M/bdt',
+        },
+        {
+            'Name' : '/9J3M/bdt',
+        },
+        {
+            'Name' : '/9J4M/bdt',
+        },
+        {
+            'Name' : '/10J2M/bdt',
+        },
+        {
+            'Name' : '/10J3M/bdt',
+        },
+        {
+            'Name' : '/10J4M/bdt',
+        }
+]
