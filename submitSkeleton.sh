61b3023cdcc72074af6aa51835924ce3b6266687
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-10-01 11:27:18 +0200

    Increase wall-time to 8hrs

--- submitSkeleton.sh
+++ submitSkeleton.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q localgrid
-#PBS -l walltime=4:00:00
+#PBS -l walltime=8:00:00
 # setting up your code and your env
 source /user/dlontkov/.bash_profile
 cd /user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016
