c0bb2f214f9a1e36981054aa2060bc44d38624fa
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-10-01 11:26:49 +0200

    Apply identical cuts to electrons and muons met>50, HT>500. HT cut is consistent with dilepton analysis

--- result/triggercuts.py
+++ result/triggercuts.py
@@ -3,11 +3,11 @@
 def trgcuts(tree_name):
 	trigger_cuts = ''
 	if 'Mu' in tree_name:
-	        trigger_cuts = "((HLT_IsoMu24==1||HLT_IsoTkMu24==1)  && met > 50 && fabs(LeptonEta)<2.1 )"
+	        trigger_cuts = "((HLT_IsoMu24==1||HLT_IsoTkMu24==1) && met > 50 && HT > 500 && fabs(LeptonEta)<2.1 )"
 	        #trigger_cuts = "(HLT_PFHT400_SixJet30_DoubleBTagCSV_p056==1)"
 	        #trigger_cuts = "(HLT_Mu15_IsoVVVL_BTagCSV_p067_PFHT400==1)"
 	elif 'El' in tree_name:
-	        trigger_cuts = "((HLT_Ele32_eta2p1_WPTight_Gsf==1) &&  met > 50 && HT > 450 && fabs(LeptonEta)<=2.1 )"
+	        trigger_cuts = "((HLT_Ele32_eta2p1_WPTight_Gsf==1) &&  met > 50 && HT > 500 && fabs(LeptonEta)<=2.1 )"
 	        #trigger_cuts = "(HLT_PFHT400_SixJet30_DoubleBTagCSV_p056==1)"
 	        #trigger_cuts = "(HLT_Ele15_IsoVVVL_PFHT350_PFMET50==1)"
 	
