45967461f699df07b172a40bdebc5952706e9236
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-10-06 00:38:40 +0200

    Improve style of the plots

--- result/tools/tdrstyle.py
+++ result/tools/tdrstyle.py
@@ -80,7 +80,8 @@ def setTDRStyle():
   # tdrStyle.SetStatY(Float_t y = 0)
 
 # Margins:
-  tdrStyle.SetPadTopMargin(0.05)
+  #tdrStyle.SetPadTopMargin(0.05)
+  tdrStyle.SetPadTopMargin(0.1)
   tdrStyle.SetPadBottomMargin(0.13)
   tdrStyle.SetPadLeftMargin(0.16)
   tdrStyle.SetPadRightMargin(0.02)
