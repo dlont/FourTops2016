831ca0cfcdcb1cc6bde48768e54b8ad84c6d1b54
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-10-06 00:43:31 +0200

    refactor. Add posibility to change prefix

--- result/makepostfitcards_mu.sh
+++ result/makepostfitcards_mu.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-folderwithmergedcraneens=plots_mu_toppt
+folderwithmergedcraneens=plots_mu_topptnonjw_v2
+prefix=ARCcomments
 targetvar=''
 ########################################
 #              MUON CARDS/POSTFIT PLOTS
 ########################################
-for targetvar in LeptonPt NjetsW multitopness HTb HTH HTX SumJetMassX 1stjetpt 2ndjetpt 5thjetpt 6thjetpt csvJetcsv3 csvJetcsv4
+#for targetvar in LeptonPt NjetsW multitopness HTb HTH HTX SumJetMassX 1stjetpt 2ndjetpt 5thjetpt 6thjetpt csvJetcsv3 csvJetcsv4
+for targetvar in 1stjetpt jetpt
 do
 	echo "$targetvar"
-	mkdir $folderwithmergedcraneens\_$targetvar
-	for i in $folderwithmergedcraneens/Cran*.root; do ln -s `readlink -f $i` $folderwithmergedcraneens\_$targetvar/`basename $i`;done
-	make -t -j card_mu.txt ERA=full INPUTLOCATION=$folderwithmergedcraneens BUILDDIR=$folderwithmergedcraneens\_$targetvar/ DATALABEL:=Single\ \#mu TREENAME=Craneen__Mu TARGETVAR=$targetvar
-	rm $folderwithmergedcraneens\_$targetvar/Hist*.root
-	make -j card_mu.txt ERA=full INPUTLOCATION=$folderwithmergedcraneens BUILDDIR=$folderwithmergedcraneens\_$targetvar/ DATALABEL:=Single\ \#mu TREENAME=Craneen__Mu TARGETVAR=$targetvar
+	targetdir=$prefix/$folderwithmergedcraneens\_$targetvar
+	mkdir $targetdir
+	for i in $folderwithmergedcraneens/Cran*.root; do ln -s `readlink -f $i` $targetdir/`basename $i`;done
+	make -t -j card_mu.txt ERA=full INPUTLOCATION=$folderwithmergedcraneens BUILDDIR=$targetdir/ DATALABEL:=Single\ \#mu TREENAME=Craneen__Mu TARGETVAR=$targetvar
+	rm $targetdir/Hist*.root
+	make -j card_mu.txt ERA=full INPUTLOCATION=$folderwithmergedcraneens BUILDDIR=$targetdir/ DATALABEL:=Single\ \#mu TREENAME=Craneen__Mu TARGETVAR=$targetvar
 done
