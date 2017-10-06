a47df425a06b1f76e1a5e1f960cecc637449ffc2
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-10-06 02:23:00 +0200

    Make output folder name configuration more flexible

--- result/makepostfitcards_mu.sh
+++ result/makepostfitcards_mu.sh
@@ -2,16 +2,16 @@
 set -x 
 
 folderwithmergedcraneens=plots_mu_topptnonjw_v2
-prefix=ARCcomments
+prefix=ARCcomments/plots_mu_topptnonjw_v2
 targetvar=''
 ########################################
 #              MUON CARDS/POSTFIT PLOTS
 ########################################
 #for targetvar in LeptonPt NjetsW multitopness HTb HTH HTX SumJetMassX 1stjetpt 2ndjetpt 5thjetpt 6thjetpt csvJetcsv3 csvJetcsv4
-for targetvar in 1stjetpt jetpt
+for targetvar in 1stjetpt jetpt HT
 do
 	echo "$targetvar"
-	targetdir=$prefix/$folderwithmergedcraneens\_$targetvar
+	targetdir=$prefix\_$targetvar
 	mkdir $targetdir
 	for i in $folderwithmergedcraneens/Cran*.root; do ln -s `readlink -f $i` $targetdir/`basename $i`;done
 	make -t -j card_mu.txt ERA=full INPUTLOCATION=$folderwithmergedcraneens BUILDDIR=$targetdir/ DATALABEL:=Single\ \#mu TREENAME=Craneen__Mu TARGETVAR=$targetvar
