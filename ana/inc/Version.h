e786601b4b48314bfb888e1abab97dbb9a863ed9
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-12-09 10:52:52 +0100

    I have to commit Version.h. to be able to rebase

--- ana/inc/Version.h
+++ ana/inc/Version.h
@@ -6,9 +6,9 @@ extern "C" {
 # endif
 
 #define GIT_BRANCH      "master"      /*! Current name of the branch     */
-#define GIT_COMMIT_HASH "71ed416" /*! Current hash of the git branch */
-#define GIT_DATE        "2017-03-30 00:12:39 +0200" /*! Current date of the git branch */
-#define GIT_TAG         "v0.0.15patch1-8-g71ed416"         /*! Current tag of the git branch  */
+#define GIT_COMMIT_HASH "b17a8cd" /*! Current hash of the git branch */
+#define GIT_DATE        "2017-12-07 18:58:09 +0100" /*! Current date of the git branch */
+#define GIT_TAG         "v0.0.31-18-gb17a8cd"         /*! Current tag of the git branch  */
 
 #if DEBUG
 #define CONF_MODE       "DEBUG" /*! Last compilation mode used */
@@ -16,7 +16,7 @@ extern "C" {
 #define CONF_MODE       "RELEASE"/*! Last compilation mode used */
 #endif
 
-#define META_INFO       "master - v0.0.15patch1-8-g71ed416 - 71ed416 - 2017-03-30 00:12:39 +0200 - "CONF_MODE /*! Version name */
+#define META_INFO       "master - v0.0.31-18-gb17a8cd - b17a8cd - 2017-12-07 18:58:09 +0100 - "CONF_MODE /*! Version name */
 
 # ifdef __cplusplus
 }
