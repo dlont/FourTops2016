e56da8de75ac96801a4b40373a3d73197a47a7e2
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2019-03-25 16:17:40 +0100

    Update /pnfs craneen output path

--- submitSkeleton.sh
+++ submitSkeleton.sh
@@ -8,7 +8,7 @@ eval `scramv1 runtime -sh`
 
 #These ENV variables are needed for gfal-copy of the output of FourTops ntupler (defaults are overriden by the FourTops application)
 export X509_USER_PROXY=/user/$USER/x509up_u$(id -u $USER)
-export PNFS_OUTPUT_DIR=srm://maite.iihe.ac.be:8443/pnfs/iihe/cms/store/user/dlontkov/clusteroutput/v0.0.39_JERSummer16_25nsV1
+export PNFS_OUTPUT_DIR=srm://maite.iihe.ac.be:8443/pnfs/iihe/cms/store/user/dlontkov/clusteroutput/v0.0.41_JERSummer16_25nsV1_4vwachira
 export FOURTOPS_OUTFILE=/scratch/$PBS_JOBID/Cran*.root
 
 # want you really want to do!!
