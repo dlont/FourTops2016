76bd271d3c8cf72dc92d329f0f458009fd6572d8
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2019-03-26 14:20:11 +0100

    Add remap histogram names tool. Useful for changing histogram systematics prefix for combination with other analysis

---
+++ result/tools/rebinhistogram/config/10J4M_rebin_to_singlebin_cff.py
@@ -0,0 +1,21 @@
+config = {
+    "file_list":[
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_mu_filt_50bins_10J4Monebin/Hists_data.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_mu_filt_50bins_10J4Monebin/Hists_T.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_mu_filt_50bins_10J4Monebin/Hists_EW.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_mu_filt_50bins_10J4Monebin/Hists_TT_CARDS.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_mu_filt_50bins_10J4Monebin/Hists_TTTT_CARDS.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_mu_filt_50bins_10J4Monebin/Hists_TTWXY.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_mu_filt_50bins_10J4Monebin/Hists_TT_HZmerged.root",
+
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_el_filt_50bins_10J4Monebin/Hists_data.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_el_filt_50bins_10J4Monebin/Hists_T.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_el_filt_50bins_10J4Monebin/Hists_EW.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_el_filt_50bins_10J4Monebin/Hists_TT_CARDS.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_el_filt_50bins_10J4Monebin/Hists_TTTT_CARDS.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_el_filt_50bins_10J4Monebin/Hists_TTWXY.root",
+        "/storage_mnt/storage/user/dlontkov/TTP_CMSSW_8_0_26_patch1/src/TopBrussels/FourTops2016/result/final_unblinding/filtered_samples/plots_el_filt_50bins_10J4Monebin/Hists_TT_HZmerged.root",
+        ],
+    "folder_list":["10J4M"],
+    "histogram":"bdt"
+}
