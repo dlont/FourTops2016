0de9b5e3df12d2944037dd2af9acde6d2d822a83
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2019-07-30 17:23:48 +0200

    Revert to CT10,NNPDF,MSTW pdf systematics for consistency with v41_weightbugfix_v3 Craneens

--- result/cards_syst_PDF.py
+++ result/cards_syst_PDF.py
@@ -2,50 +2,50 @@ from centralweight import centralweight
 def getPDFCutSets(syst,cut_string,search_scheme='9J3M,10J3M'):
     cut_sets = [
         #central
-        ("allSF_{0}Up".format(syst), "inclusive",  "(1 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("allSF_{0}Down".format(syst), "inclusive",  "(1 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
+        ("allSF_{0}Up".format(syst), "inclusive",  "(1 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("allSF_{0}Down".format(syst), "inclusive",  "(1 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
 
-        ("6J2M_{0}Up".format(syst), "Njet=6, nMtags=2",  "(nJets==6 && nMtags==2 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("6J3M_{0}Up".format(syst), "Njet=6, nMtags=3",  "(nJets==6 && nMtags==3 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("6J4M_{0}Up".format(syst), "Njet=6, nMtags=4",  "(nJets==6 && nMtags>=4 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("7J2M_{0}Up".format(syst), "Njet=7, nMtags=2",  "(nJets==7 && nMtags==2 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("7J3M_{0}Up".format(syst), "Njet=7, nMtags=3",  "(nJets==7 && nMtags==3 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("7J4M_{0}Up".format(syst), "Njet=7, nMtags=4",  "(nJets==7 && nMtags>=4 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("8J2M_{0}Up".format(syst), "Njet=8, nMtags=2",  "(nJets==8 && nMtags==2 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("8J3M_{0}Up".format(syst), "Njet=8, nMtags=3",  "(nJets==8 && nMtags==3 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("8J4M_{0}Up".format(syst), "Njet=8, nMtags=4",  "(nJets==8 && nMtags>=4 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("9J2M_{0}Up".format(syst), "Njet=9, nMtags=2", "(nJets==9 && nMtags==2 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-        ("10J2M_{0}Up".format(syst), "Njet=9+, nMtags=2", "(nJets>9 && nMtags==2 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
+        ("6J2M_{0}Up".format(syst), "Njet=6, nMtags=2",  "(nJets==6 && nMtags==2 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("6J3M_{0}Up".format(syst), "Njet=6, nMtags=3",  "(nJets==6 && nMtags==3 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("6J4M_{0}Up".format(syst), "Njet=6, nMtags=4",  "(nJets==6 && nMtags>=4 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("7J2M_{0}Up".format(syst), "Njet=7, nMtags=2",  "(nJets==7 && nMtags==2 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("7J3M_{0}Up".format(syst), "Njet=7, nMtags=3",  "(nJets==7 && nMtags==3 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("7J4M_{0}Up".format(syst), "Njet=7, nMtags=4",  "(nJets==7 && nMtags>=4 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("8J2M_{0}Up".format(syst), "Njet=8, nMtags=2",  "(nJets==8 && nMtags==2 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("8J3M_{0}Up".format(syst), "Njet=8, nMtags=3",  "(nJets==8 && nMtags==3 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("8J4M_{0}Up".format(syst), "Njet=8, nMtags=4",  "(nJets==8 && nMtags>=4 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("9J2M_{0}Up".format(syst), "Njet=9, nMtags=2", "(nJets==9 && nMtags==2 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+        ("10J2M_{0}Up".format(syst), "Njet=9+, nMtags=2", "(nJets>9 && nMtags==2 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
 
-        ("6J2M_{0}Down".format(syst), "Njet=6, nMtags=2",  "(nJets==6 && nMtags==2 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("6J3M_{0}Down".format(syst), "Njet=6, nMtags=3",  "(nJets==6 && nMtags==3 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("6J4M_{0}Down".format(syst), "Njet=6, nMtags=4",  "(nJets==6 && nMtags>=4 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("7J2M_{0}Down".format(syst), "Njet=7, nMtags=2",  "(nJets==7 && nMtags==2 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("7J3M_{0}Down".format(syst), "Njet=7, nMtags=3",  "(nJets==7 && nMtags==3 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("7J4M_{0}Down".format(syst), "Njet=7, nMtags=4",  "(nJets==7 && nMtags>=4 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("8J2M_{0}Down".format(syst), "Njet=8, nMtags=2",  "(nJets==8 && nMtags==2 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("8J3M_{0}Down".format(syst), "Njet=8, nMtags=3",  "(nJets==8 && nMtags==3 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("8J4M_{0}Down".format(syst), "Njet=8, nMtags=4",  "(nJets==8 && nMtags>=4 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("9J2M_{0}Down".format(syst), "Njet=9, nMtags=2",  "(nJets==9 && nMtags==2 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-        ("10J2M_{0}Down".format(syst), "Njet=9+, nMtags=2", "(nJets>9 && nMtags==2 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
+        ("6J2M_{0}Down".format(syst), "Njet=6, nMtags=2",  "(nJets==6 && nMtags==2 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("6J3M_{0}Down".format(syst), "Njet=6, nMtags=3",  "(nJets==6 && nMtags==3 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("6J4M_{0}Down".format(syst), "Njet=6, nMtags=4",  "(nJets==6 && nMtags>=4 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("7J2M_{0}Down".format(syst), "Njet=7, nMtags=2",  "(nJets==7 && nMtags==2 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("7J3M_{0}Down".format(syst), "Njet=7, nMtags=3",  "(nJets==7 && nMtags==3 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("7J4M_{0}Down".format(syst), "Njet=7, nMtags=4",  "(nJets==7 && nMtags>=4 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("8J2M_{0}Down".format(syst), "Njet=8, nMtags=2",  "(nJets==8 && nMtags==2 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("8J3M_{0}Down".format(syst), "Njet=8, nMtags=3",  "(nJets==8 && nMtags==3 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("8J4M_{0}Down".format(syst), "Njet=8, nMtags=4",  "(nJets==8 && nMtags>=4 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("9J2M_{0}Down".format(syst), "Njet=9, nMtags=2",  "(nJets==9 && nMtags==2 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+        ("10J2M_{0}Down".format(syst), "Njet=9+, nMtags=2", "(nJets>9 && nMtags==2 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
     ]
     if search_scheme == '9J3M,10J3M':
             cut_sets += [
-            ("9J3M_{0}Up".format(syst), "Njet=9, nMtags=3", "(nJets==9 && nMtags>=3 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-            ("10J3M_{0}Up".format(syst), "Njet=9+, nMtags=3", "(nJets>9 && nMtags>=3 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-            ("9J3M_{0}Down".format(syst), "Njet=9, nMtags=3",  "(nJets==9 && nMtags>=3 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-            ("10J3M_{0}Down".format(syst), "Njet=9+, nMtags=3", "(nJets>9 && nMtags>=3 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
+            ("9J3M_{0}Up".format(syst), "Njet=9, nMtags=3", "(nJets==9 && nMtags>=3 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+            ("10J3M_{0}Up".format(syst), "Njet=9+, nMtags=3", "(nJets>9 && nMtags>=3 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+            ("9J3M_{0}Down".format(syst), "Njet=9, nMtags=3",  "(nJets==9 && nMtags>=3 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+            ("10J3M_{0}Down".format(syst), "Njet=9+, nMtags=3", "(nJets>9 && nMtags>=3 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
 		]
     elif search_scheme == '9J4M,10J4M':
             cut_sets += [
-            ("9J3M_{0}Up".format(syst), "Njet=9, nMtags=3", "(nJets==9 && nMtags==3 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-            ("9J4M_{0}Up".format(syst), "Njet=9, nMtags=4", "(nJets==9 && nMtags>=4 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-            ("10J3M_{0}Up".format(syst), "Njet=9+, nMtags=3", "(nJets>9 && nMtags==3 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-            ("10J4M_{0}Up".format(syst), "Njet=9+, nMtags=4", "(nJets>9 && nMtags>=4 {0})*{1}*Max$(pdf_nnpdf)".format(cut_string,centralweight)),
-            ("9J3M_{0}Down".format(syst), "Njet=9, nMtags=3",  "(nJets==9 && nMtags==3 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-            ("9J4M_{0}Down".format(syst), "Njet=9, nMtags=4",  "(nJets==9 && nMtags>=4 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-            ("10J3M_{0}Down".format(syst), "Njet=9+, nMtags=3", "(nJets>9 && nMtags==3 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight)),
-            ("10J4M_{0}Down".format(syst), "Njet=9+, nMtags=4", "(nJets>9 && nMtags>=4 {0})*{1}*MinIf$(pdf_nnpdf,pdf_nnpdf>0)".format(cut_string,centralweight))
+            ("9J3M_{0}Up".format(syst), "Njet=9, nMtags=3", "(nJets==9 && nMtags==3 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+            ("9J4M_{0}Up".format(syst), "Njet=9, nMtags=4", "(nJets==9 && nMtags>=4 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+            ("10J3M_{0}Up".format(syst), "Njet=9+, nMtags=3", "(nJets>9 && nMtags==3 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+            ("10J4M_{0}Up".format(syst), "Njet=9+, nMtags=4", "(nJets>9 && nMtags>=4 {0})*{1}*Max$(abs(pdfw))".format(cut_string,centralweight)),
+            ("9J3M_{0}Down".format(syst), "Njet=9, nMtags=3",  "(nJets==9 && nMtags==3 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+            ("9J4M_{0}Down".format(syst), "Njet=9, nMtags=4",  "(nJets==9 && nMtags>=4 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+            ("10J3M_{0}Down".format(syst), "Njet=9+, nMtags=3", "(nJets>9 && nMtags==3 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight)),
+            ("10J4M_{0}Down".format(syst), "Njet=9+, nMtags=4", "(nJets>9 && nMtags>=4 {0})*{1}*Min$(abs(pdfw))".format(cut_string,centralweight))
 		]
     return cut_sets
 
