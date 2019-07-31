88c929bf98625735a92a31210f7233f799c5a10c
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2019-07-31 14:34:03 +0200

    Update versions and paths

--- ana/inc/Version.h
+++ ana/inc/Version.h
@@ -5,10 +5,10 @@
 extern "C" {
 # endif
 
-#define GIT_BRANCH      "master"      /*! Current name of the branch     */
-#define GIT_COMMIT_HASH "b17a8cd" /*! Current hash of the git branch */
-#define GIT_DATE        "2017-12-07 18:58:09 +0100" /*! Current date of the git branch */
-#define GIT_TAG         "v0.0.31-18-gb17a8cd"         /*! Current tag of the git branch  */
+#define GIT_BRANCH      "v43ttccsplit"      /*! Current name of the branch     */
+#define GIT_COMMIT_HASH "bf6a7bc" /*! Current hash of the git branch */
+#define GIT_DATE        "2019-07-30 17:35:24 +0200" /*! Current date of the git branch */
+#define GIT_TAG         "v0.0.43ttccsplit-1-gbf6a7bc"         /*! Current tag of the git branch  */
 
 #if DEBUG
 #define CONF_MODE       "DEBUG" /*! Last compilation mode used */
@@ -16,7 +16,7 @@ extern "C" {
 #define CONF_MODE       "RELEASE"/*! Last compilation mode used */
 #endif
 
-#define META_INFO       "master - v0.0.31-18-gb17a8cd - b17a8cd - 2017-12-07 18:58:09 +0100 - "CONF_MODE /*! Version name */
+#define META_INFO       "v43ttccsplit - v0.0.43ttccsplit-1-gbf6a7bc - bf6a7bc - 2019-07-30 17:35:24 +0200 - "CONF_MODE /*! Version name */
 
 # ifdef __cplusplus
 }
